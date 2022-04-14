3a53a31ddbbf6c701d3af28744f62d8440909935
Gupelly <timofejirgashev@gmail.com>
2022-04-14 17:53:53 +0500

    Add Hashcode

--- FactorioWiki/FactorioWiki/FactorioWiki/FactorioItem.cs
+++ FactorioWiki/FactorioWiki/FactorioWiki/FactorioItem.cs
@@ -6,25 +6,28 @@ namespace FactorioWiki
 {
     internal class FactorioItem
     {
-        public int Quantity { get; set; }
-        public int Id { get; set; } //Не используется
+        //public int Quantity { get; set; }
+        //public int Hashcode { get; set; } 
         public string ItemName { get; set; }
         public string Description { get; set; }
         public string Picture { get; set; }
         public FactorioItem[] ResoursesToCraft { get; set; }
-        public bool IsCrafable { get; set; }
+        public bool IsCraftable { get; set; }
+
+        public Dictionary<FactorioItem, int> Link;
         /// <summary>
         /// Предмет который нельзя скрафтить
         /// </summary>
         /// <param name="ItemName"> Имя предмета</param>
         /// <param name="Description"> Описание</param>
         /// <param name="Picture"> Ссылка на картинку</param>
-        public FactorioItem(string ItemName, string Description, string Picture)
+        public FactorioItem(string ItemName, string Description, string Picture, bool isCraftable = false)
         {
             this.ItemName = ItemName;
             this.Description = Description;
             this.Picture = Picture;
-            IsCrafable = false;
+            IsCraftable = isCraftable;
+            //Hashcode = GetHash();
         }
         /// <summary>
         /// Предмет который можно скрафтить
@@ -38,7 +41,7 @@ namespace FactorioWiki
             this.ItemName = ItemName;
             this.Description = Description;
             this.Picture = Picture;
-            IsCrafable = true;
+            IsCraftable = true;
             this.ResoursesToCraft = ResoursesToCraft;
         }
         /// <summary>
@@ -46,13 +49,30 @@ namespace FactorioWiki
         /// </summary>
         /// <param name="item">Предмет</param>
         /// <param name="quant">Кол-во</param>
-        public FactorioItem(FactorioItem item, int quant)
-        {
-            ItemName = item.ItemName;
-            Description = item.Description;
-            Picture = item.Picture;
-            IsCrafable = item.IsCrafable;
-            Quantity = quant;
-        }
+        //public FactorioItem(FactorioItem item, int quant)
+        //{
+        //    ItemName = item.ItemName;
+        //    Description = item.Description;
+        //    Picture = item.Picture;
+        //    IsCrafable = item.IsCrafable;
+        //    Quantity = quant;
+        //}
+
+        //private int GetHash()
+        //{
+        //    unchecked
+        //    {
+        //        var prime = 16777619;
+        //        var hash = (long)2166136261;
+        //        foreach (var @char in ItemName)
+        //            hash = (hash ^ @char) * prime;
+        //        return (int)hash;
+        //    }
+        //}
+
+        //public override int GetHashCode()
+        //{
+        //    return Hashcode;
+        //}
     }
 }
