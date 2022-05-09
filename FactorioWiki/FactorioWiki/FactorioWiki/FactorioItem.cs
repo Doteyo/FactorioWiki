e90c28039315a31b6d7edbb385fc163f8bd9d3db
Gupelly <timofejirgashev@gmail.com>
2022-05-10 00:39:28 +0500

    A

--- FactorioWiki/FactorioWiki/FactorioWiki/FactorioItem.cs
+++ FactorioWiki/FactorioWiki/FactorioWiki/FactorioItem.cs
@@ -4,12 +4,20 @@ using System.Text;
 
 namespace FactorioWiki
 {
+    public class Technology
+    {
+
+    }
+
     public class FactorioItem
     {
         public string ItemName { get; set; }
         public string ShortDes { get; set; }
         public string LongDes { get; set; }
+        public double Time { get; set; }
         public string Picture { get; set; }
+        public Technology Technology { get; set; }
+        public FactorioItem[] Factory { get; set; }
         public (int, FactorioItem)[] ResoursesToCraft { get; set; }
 
         /// <summary>
@@ -17,9 +25,22 @@ namespace FactorioWiki
         /// </summary>
         /// <param name="ItemName"> Имя предмета</param>
         /// <param name="Description"> Описние</param>
+        /// <param name="Time"> Время создания</param>
         /// <param name="Picture"> Ссылка на картинку</param>
+        /// <param name="Technology"> Последняя необходимая технология</param>
+        /// <param name="Factory"> На чем создавать предмет</param>
         /// <param name="ResoursesToCraft"> Предметы из котрых крафтиться</param>
-        public FactorioItem(string ItemName, string ShortDes, string LongDes, string Picture, params (int,FactorioItem)[] ResoursesToCraft)
+        public FactorioItem(string ItemName, string ShortDes, string LongDes, double time, string Picture, params (int,FactorioItem)[] ResoursesToCraft)
+        {
+            this.ItemName = ItemName;
+            this.ShortDes = ShortDes;
+            this.LongDes = LongDes;
+            Time = time;
+            this.Picture = Picture;
+            this.ResoursesToCraft = ResoursesToCraft;
+        }
+
+        public FactorioItem(string ItemName, string ShortDes, string LongDes, string Picture, double time = 0, FactorioItem[] factory = null, params (int, FactorioItem)[] ResoursesToCraft)
         {
             this.ItemName = ItemName;
             this.ShortDes = ShortDes;
